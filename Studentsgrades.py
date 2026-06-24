1bee3558de1ed6560409f007b983c3e87b3850a5
Ajmal Gazali <ajmalgazaliit@gmail.com>
2026-06-24 20:59:11 +0530

    Add files via upload
    A simple python project that calculates and display student grades based on their marks.
    This project demonstrate that use of conditional the use of conditional statements, user input
    handling, and basic programming logic.

---
+++ Studentsgrades.py
@@ -0,0 +1,11 @@
+marks1=int(input("Enter a marks : "))
+marks2=int(input("Enter a marks : "))
+marks3=int(input("Enter a marks : "))
+
+total_percentage=(100*(marks1+marks2+marks3))/300
+
+if (total_percentage>40):
+    print("you are passed ;",total_percentage)
+
+else:
+    ("yor are failed \n Try next year😥!!")
